2e148b6301f6c38f7736ce28db34cc8b043b88e6
Johannes Schobel <johannes.schobel@googlemail.com>
2018-07-03 18:00:36 +0200

    test user with access

--- src/Abstracts/Tests/Cests/BaseCest.php
+++ src/Abstracts/Tests/Cests/BaseCest.php
@@ -4,12 +4,14 @@ namespace HiveApi\Core\Abstracts\Tests\Cests;
 
 use HiveApi\Core\Traits\Tests\TestsAuthenticationHelperTrait;
 use HiveApi\Core\Traits\Tests\TestsResponseHelperTrait;
+use HiveApi\Core\Traits\Tests\TestsStubHelperTrait;
 use HiveApi\Core\Traits\Tests\TestsUserHelperTrait;
 
 abstract class BaseCest
 {
     use TestsAuthenticationHelperTrait;
     use TestsResponseHelperTrait;
+    use TestsStubHelperTrait;
     use TestsUserHelperTrait;
 
 }
--- src/Traits/Tests/TestsUserHelperTrait.php
+++ src/Traits/Tests/TestsUserHelperTrait.php
@@ -5,22 +5,25 @@ namespace HiveApi\Core\Traits\Tests;
 use App\Containers\User\Data\Repositories\UserRepository;
 use App\Containers\User\Models\User;
 use Faker\Factory;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Hash;
 
 trait TestsUserHelperTrait
 {
 
     /**
-     * Returns a specific test user. If it does not exist, it creates a new user!
+     * Creates a new user and assigns specific roles / permissions to the latter
      *
      * @param array $data
+     * @param null  $access
      *
      * @return mixed
      */
-    protected function getTestingUser(array $data = [])
+    protected function getTestingUser(array $data = [], $access = null)
     {
-        return $this->createTestingUser($data);
+        $user = $this->createTestingUser($data);
+        $user = $this->grantAccess($user, $access);
+
+        return $user;
     }
 
     private function createTestingUser(array $userdata = [])
@@ -30,20 +33,88 @@ trait TestsUserHelperTrait
 
     private function prepareUserDetails(array $userdata = [])
     {
-        $faker = Factory::create();
-
-        $defaultUserDetails = [
-            'name'     => $faker->name,
-            'email'    => $faker->email,
-            'password' => 'password',
-            'confirmed' => true,
-        ];
+        $defaultUserDetails = $this->getDefaultUserData();
 
         $finalUserData = array_merge($defaultUserDetails, $userdata);
 
         $finalUserData['password'] = Hash::make($finalUserData['password']);
 
         return $finalUserData;
+    }
+
+    /**
+     * Define the default attributes of a user
+     *
+     * @return array
+     */
+    private function getDefaultUserData()
+    {
+        $faker = Factory::create();
+
+        return [
+            'name'     => $faker->name,
+            'email'    => $faker->email,
+            'password' => 'password',
+            'confirmed' => true,
+        ];
+    }
+
+    /**
+     * Define the default role / permissions for a user
+     *
+     * @return array
+     */
+    private function getDefaultAccess()
+    {
+        return [
+            'roles' => [],
+            'permissions' => [],
+        ];
+    }
+
+    protected function grantAccess(User $user, $access = null)
+    {
+        $defaultAccess = $this->getDefaultAccess();
+
+        if ($access === null) {
+            $access = [];
+        }
+
+        $access = array_merge($defaultAccess, $access);
+
+        $this->grantRoleAccessToUser($user, $access['roles']);
+        $this->grantPermissionAccessToUser($user, $access['permissions']);
+
+        $user->refresh();
+        return $user;
+    }
+
+    /**
+     * Grants the user specific roles
+     *
+     * @param User  $user
+     * @param array $roles
+     */
+    private function grantRoleAccessToUser(User $user, array $roles = [])
+    {
+        foreach ($roles as $role)
+        {
+            $user->assignRole($role);
+        }
+    }
+
+    /**
+     * Grants the user specific permissions
+     *
+     * @param User  $user
+     * @param array $permissions
+     */
+    private function grantPermissionAccessToUser(User $user, array $permissions = [])
+    {
+        foreach ($permissions as $permission)
+        {
+            $user->givePermissionTo($permission);
+        }
     }
 
 }
